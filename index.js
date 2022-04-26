a0950a4a4864283660eac35cafc864719b6cb7d3
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-04-25 17:55:03 -0700

    commit tree aeae10bf6502c84dba54254c0f7ca9f47b47ff60

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 15.5;
+const num2 = 4;
+const multiply = num1 * num2;
+
+const random = Math.floor((Math.random() * 10) + 1);
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 3, 20, 16);
